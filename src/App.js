93b1f634320295038416a44cbaead5df604ba83e
Nitesh Khandelwal <Nitesh.Khandelwal@team.telstra.com>
2020-10-10 12:30:52 +0530

    part-1 Navigation

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navaigation from './components/navigation'
+import {BrowserRouter as Router,Switch,Route,Redirect} from'react-router-dom'
+import PageRenderer from './page-renderer'
+function App() {
+  return (
+    <Router>
+    <div className="App">
+     <Navaigation /> 
+     <Switch>
+       <Route path="/:page" component={PageRenderer}/>
+       <Route path="/" render={()=><Redirect to="home"/>}/>
+       <Route component={()=>404}/>
+     </Switch>
+    </div>
+    </Router>
+  )
+}
+
+export default App
